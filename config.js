41fe29ac2dc5cebc34b82c9b7e1ebb4987ab0782
suraj singh <sks19980815@gmail.com>
2021-12-03 22:10:32 +0530

    updated config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     secret: "secretString",
-    database: "mongodb://delta:delta!1234@3.21.105.155.:27017/deltaPromotions", // Devlopment
+    database: "mongodb://delta:delta!1234@3.21.105.155:27017/deltaPromotions", // Devlopment
     username: "Homelab",
     password: "H@6125b"
 };
