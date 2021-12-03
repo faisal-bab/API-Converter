1cf179368f46e42fd10264a8a6d4767156ed54cd
suraj singh <sks19980815@gmail.com>
2021-12-03 22:59:43 +0530

    updated config

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
     });
 });
 //-----------------------------//
-const running_port = process.env.PORT || 80;
+const running_port = process.env.PORT || 8080;
 
 http.createServer({}, app).listen(running_port);
 
